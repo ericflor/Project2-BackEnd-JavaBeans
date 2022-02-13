a9685b0b85be1351d5314f8ddbbba2b120165231
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-02-13 14:02:11 -0500

    implemented lombok

--- src/main/java/com/revature/models/Group.java
+++ src/main/java/com/revature/models/Group.java
@@ -1,6 +1,9 @@
 package com.revature.models;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
@@ -8,6 +11,9 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "group_tbl")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Group {
 
     @Id
@@ -22,58 +28,58 @@ public class Group {
 //    @JsonManagedReference
 //    private List<Decisions> decisions;
 
-    public Group() {
-    }
-
-    public Group(int id, String name, boolean open) {
-        this.id = id;
-        this.name = name;
-        this.open = open;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isOpen() {
-        return open;
-    }
-
-    public void setOpen(boolean open) {
-        this.open = open;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Group)) return false;
-        Group group = (Group) o;
-        return getId() == group.getId() && isOpen() == group.isOpen() && Objects.equals(getName(), group.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), isOpen());
-    }
-
-    @Override
-    public String toString() {
-        return "Group{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", open=" + open +
-                '}';
-    }
+//    public Group() {
+//    }
+//
+//    public Group(int id, String name, boolean open) {
+//        this.id = id;
+//        this.name = name;
+//        this.open = open;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public boolean isOpen() {
+//        return open;
+//    }
+//
+//    public void setOpen(boolean open) {
+//        this.open = open;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Group)) return false;
+//        Group group = (Group) o;
+//        return getId() == group.getId() && isOpen() == group.isOpen() && Objects.equals(getName(), group.getName());
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId(), getName(), isOpen());
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Group{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", open=" + open +
+//                '}';
+//    }
 }
