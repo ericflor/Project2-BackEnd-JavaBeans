f6b751f58daad18885561673ffadedd53fb01157
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-02-20 17:16:56 -0500

    added update user

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.revature.services.UserService;
 import com.revature.utils.CookiesUtil;
 import com.revature.utils.Encryption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +37,26 @@ public class UserController {
         return ResponseEntity.status(400).build();
     }
 
+    @PostMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        System.out.println("==================HELLOOO====================");
+
+        if(user.getPassword()==""){
+            User user2 = userService.getUser(user.getId());
+            user.setPassword(user2.getPassword());
+        }else{
+            user.setPassword(Encryption.stringToMD5(user.getPassword()));
+        }
+
+        if(userService.addOrUpdateUser(user)){
+            user.setPassword("");
+            ResponseCookie cookie = CookiesUtil.buildResponseCookie(user);
+
+            return ResponseEntity.status(201).header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
+        }
+        return ResponseEntity.status(400).build();
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@CookieValue(name = "upNext_user") String cookie, @PathVariable("id") int id){
 
@@ -59,6 +81,7 @@ public class UserController {
         User user = CookiesUtil.isCookieValid(cookie);
         if(user!=null){
             user = userService.getUser(user.getId());
+            user.setPassword("");
             return ResponseEntity.status(200).body(user);
         }
         return ResponseEntity.status(401).build();
